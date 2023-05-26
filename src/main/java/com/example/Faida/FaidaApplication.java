ce3545c89dd90e22c2c5db1c972e6afdeba112f0
MosesOchieng <mosesochiengopiyo@gmail.com>
2023-05-26 15:53:14 +0300

    push commit

---
+++ src/main/java/com/example/Faida/FaidaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Faida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaidaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaidaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Faida/FaidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Faida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
